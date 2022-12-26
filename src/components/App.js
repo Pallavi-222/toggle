afe9fcaa20bb5227d757111964cd8dc8fa46266a
Pallavi-222 <120183083+Pallavi-222@users.noreply.github.com>
2022-12-26 22:52:27 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import '../styles/App.css';
+import { Post } from './Post';
+import { PostList } from './PostList';
+const App = () => {
+  
+  return (
+    <div id="main">
+      <PostList />
+    </div>
+  )
+}
+
+
+export default App;
